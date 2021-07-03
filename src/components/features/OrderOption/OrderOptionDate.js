4dfd22c894e29415d59fb28998ab6856ca738cc4
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2021-07-03 15:48:21 +0200

    Add Date and Text OrderOptions

---
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+const OrderOptionDate = ({currentValue, setOptionValue}) => (
+  <DatePicker selected={currentValue} onChange={setOptionValue} />
+);
+
+OrderOptionDate.propTypes = {
+  currentValue: PropTypes.any,
+  setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionDate;
---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './OrderOption.scss';
+
+const OrderOptionText = ({currentValue, setOptionValue}) => (
+  <input
+    className={styles.input}
+    value={currentValue}
+    onChange={event => setOptionValue(event.currentTarget.value)}
+  />
+);
+
+OrderOptionText.propTypes = {
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionText;
